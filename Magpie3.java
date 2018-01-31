2d51e556f5442588926a0acc1740c1bc24f882fa
williamtrang <32171517+williamtrang@users.noreply.github.com>
2018-01-31 01:46:19 -0800

    Add files via upload

--- Magpie3.java
+++ Magpie3.java
@@ -1,41 +1,13 @@
-/**
- * A program to carry on conversations with a human user.
- * This version: 
- * <ul><li>
- *    Uses advanced search for keywords 
- * </li></ul> 
- *    
- * @author Laurie White
- * @version April 2012
- */
-public class Magpie3
-{
-	/**
-	 * Get a default greeting
-	 * 
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
+public class Magpie3 {
+	public String getGreeting() {
 		return "Hello, let's talk.";
 	}
-
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
+	public String getResponse(String statement) {
 		String response = "";
-		if (statement.length() == 0)
-		{
+		if (statement.length() == 0) {
 			response = "Say something, please.";
 		}
-		else if (findKeyword(statement, "no") >= 0)
-		{
+		else if (findKeyword(statement, "no") >= 0) {
 			response = "Why so negative?";
 		}
 		else if (findKeyword(statement, "mother") >= 0
@@ -45,59 +17,43 @@ public class Magpie3
 		{
 			response = "Tell me more about your family.";
 		}
-		else
-		{
+		else if(findKeyword(statement,"dog")>=0||findKeyword(statement,"cat")>=0){
+			response = "Tell me more about your pets.";
+		}
+		else if(findKeyword(statement,"Zheng")>=0){
+			response = "She sounds pretty radical.";
+		}
+		else if(statement.trim().isEmpty()){
+			response = "Say something, please.";
+		}
+		else if(findKeyword(statement,"physics")>=0) {
+			response = "Don't talk to me ever again";
+		}
+		else if(findKeyword(statement,"rice")>=0) {
+			response = "I like rice.";
+		}
+		else if(findKeyword(statement,"Keith")>=0){
+			response = "I do Tae Keith Do";
+		}
+		else {
 			response = getRandomResponse();
 		}
 		return response;
 	}
-
-	/**
-	 * Search for one word in phrase. The search is not case
-	 * sensitive. This method will check that the given goal
-	 * is not a substring of a longer string (so, for
-	 * example, "I know" does not contain "no").
-	 *
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @param startPos
-	 *            the character of the string to begin the
-	 *            search at
-	 * @return the index of the first occurrence of goal in
-	 *         statement or -1 if it's not found
-	 */
-	private int findKeyword(String statement, String goal,
-			int startPos)
-	{
+	private int findKeyword(String statement, String goal, int startPos) {
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
-
-		// The only change to incorporate the startPos is in
-		// the line below
 		int psn = phrase.indexOf(goal, startPos);
-
-		// Refinement--make sure the goal isn't part of a
-		// word
-		while (psn >= 0)
-		{
-			// Find the string of length 1 before and after
-			// the word
+		while (psn >= 0) {
 			String before = " ", after = " ";
-			if (psn > 0)
-			{
+			if (psn > 0) {
 				before = phrase.substring(psn - 1, psn);
 			}
-			if (psn + goal.length() < phrase.length())
-			{
+			if (psn + goal.length() < phrase.length()) {
 				after = phrase.substring(
 						psn + goal.length(),
 						psn + goal.length() + 1);
 			}
-
-			// If before and after aren't letters, we've
-			// found the word
 			if (((before.compareTo("a") < 0) || (before
 					.compareTo("z") > 0)) // before is not a
 											// letter
@@ -106,65 +62,30 @@ public class Magpie3
 			{
 				return psn;
 			}
-
-			// The last position didn't work, so let's find
-			// the next, if there is one.
 			psn = phrase.indexOf(goal, psn + 1);
-
 		}
-
 		return -1;
 	}
-
-	/**
-	 * Search for one word in phrase. The search is not case
-	 * sensitive. This method will check that the given goal
-	 * is not a substring of a longer string (so, for
-	 * example, "I know" does not contain "no"). The search
-	 * begins at the beginning of the string.
-	 * 
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @return the index of the first occurrence of goal in
-	 *         statement or -1 if it's not found
-	 */
-	private int findKeyword(String statement, String goal)
-	{
+	private int findKeyword(String statement, String goal) {
 		return findKeyword(statement, goal, 0);
 	}
-
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * 
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
+	private String getRandomResponse() {
 		final int NUMBER_OF_RESPONSES = 4;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
-
-		if (whichResponse == 0)
-		{
+		if (whichResponse == 0) {
 			response = "Interesting, tell me more.";
 		}
-		else if (whichResponse == 1)
-		{
+		else if (whichResponse == 1) {
 			response = "Hmmm.";
 		}
-		else if (whichResponse == 2)
-		{
+		else if (whichResponse == 2) {
 			response = "Do you really think so?";
 		}
-		else if (whichResponse == 3)
-		{
+		else if (whichResponse == 3) {
 			response = "You don't say.";
 		}
-
 		return response;
 	}
-
 }
